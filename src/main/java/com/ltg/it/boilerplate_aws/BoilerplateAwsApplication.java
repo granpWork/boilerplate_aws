cd26a9f9c35d2acc67dcae6df4ff485f35aa8e14
granpWork <granp.work@gmail.com>
2025-01-30 10:22:57 +0800

    first commit

---
+++ src/main/java/com/ltg/it/boilerplate_aws/BoilerplateAwsApplication.java
@@ -0,0 +1,13 @@
+package com.ltg.it.boilerplate_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ltg/it/boilerplate_aws/BoilerplateAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltg.it.boilerplate_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
